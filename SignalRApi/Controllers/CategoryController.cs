e0c095b755f7faea42ce3e5f95b965ba9ed85ae8
saltukyasar <saltukyasar@gmail.com>
2024-04-12 14:00:38 +0200

    Contact and Discount API are done

---
+++ SignalRApi/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList() 
+        {
+            var value = _mapper.Map<List<ResultCategoryDto>>(_categoryService.TGetListAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            _categoryService.TAdd(new Category()
+            {
+                CategoryName = createCategoryDto.CategoryName,
+                Status = createCategoryDto.Status,
+            });
+            return Ok("Category is added");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _categoryService.TGetByID(id);
+            _categoryService.TDelete(value);
+            return Ok("Category is deleted");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _categoryService.TGetByID(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            _categoryService.TUpdate(new Category()
+            {
+                CategoryName = updateCategoryDto.CategoryName,
+                Status = updateCategoryDto.Status,
+                CategoryID = updateCategoryDto.CategoryID,
+            });
+            return Ok("Category is updated");
+        }
+    }
+}
