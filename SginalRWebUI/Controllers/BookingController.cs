f1909ba55f1087cb6417775718d51567d52e21cc
saltukyasar <saltukyasar@gmail.com>
2024-05-06 16:19:08 +0200

    booking is created

--- SginalRWebUI/Controllers/BookingController.cs
+++ SginalRWebUI/Controllers/BookingController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BookingDtos;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
@@ -18,9 +21,34 @@ namespace SignalRWebUI.Controllers
             if(responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
             }
             return View();
         }
+
+        [HttpGet]
+        public IActionResult CreateBooking()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking(CreateBookingDto createBookingDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7252/api/Booking", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
     }
 }
